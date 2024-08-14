40e665f3ce67db8058afeb689457234faf0a04f8
Paolo Belforte <pbelforte2003@gmail.com>
2024-08-14 11:32:54 -0300

    Disabled input buffering

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,22 @@ use lc3_vm::command_line_parser::parser::{
     debug_program_from_file, execute_program_from_file, execute_vm_in_interactive_mode,
     receive_command_line_arguments,
 };
+use termios::{tcsetattr, Termios, ECHO, ICANON, TCSANOW};
+
+fn disable_input_buffering() -> Result<(), String> {
+    let mut termios = Termios::from_fd(0).map_err(|error| error.to_string())?;
+
+    termios.c_lflag &= !ICANON & !ECHO;
+
+    tcsetattr(0, TCSANOW, &mut termios).map_err(|error| error.to_string())?;
+
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
     let args = receive_command_line_arguments()?;
+
+    disable_input_buffering()?;
 
     if let Some(file) = args.get_one::<String>("file") {
         execute_program_from_file(file)?;
