fc580fcf5a00c90b98cfca529067a7ffd164c1dc
Paolo Belforte <75280184+belfortep@users.noreply.github.com>
2024-08-14 09:26:09 -0300

    Fix typo in tests names
    Co-authored-by: Juan Ignacio Medone Sabatini <jmedone@fi.uba.ar>

--- src/virtual_machine/lc3_virtual_machine.rs
+++ src/virtual_machine/lc3_virtual_machine.rs
@@ -215,7 +215,7 @@ pub mod test {
     }
 
     #[test]
-    fn can_add_two_numbers_in_differents_registers() {
+    fn can_add_two_numbers_in_different_registers() {
         let mut virtual_machine = LC3VirtualMachine::new(0);
         let add_one_to_register_zero = 0b0001_000_000_1_00001;
         let add_one_to_register_one = 0b0001_001_001_1_00001;
