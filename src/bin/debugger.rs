595d80d617b916e7c11e03ccbdfad7e2493ebc4f
Paolo Belforte <pbelforte2003@gmail.com>
2024-08-15 09:23:48 -0300

    Moved debugger address to constants

--- src/bin/debugger.rs
+++ src/bin/debugger.rs
@@ -2,6 +2,8 @@ use std::{
     io::{stdin, BufRead, BufReader, Write},
     net::TcpStream,
 };
+const LOCAL_HOST: &str = "127.0.0.1";
+const PORT: &str = "3000";
 
 fn print_instructions() {
     println!("Instructions: ");
@@ -12,14 +14,15 @@ fn print_instructions() {
 }
 
 fn main() -> Result<(), String> {
-    let stream = TcpStream::connect("127.0.0.1:3000").map_err(|error| error.to_string())?;
+    let connection_address = format!("{}:{}", LOCAL_HOST, PORT);
+    let stream = TcpStream::connect(connection_address).map_err(|error| error.to_string())?;
     print_instructions();
-    let mut reader = BufReader::new(&stream);
+    let mut connection_reader = BufReader::new(&stream);
     for line in stdin().lock().lines() {
         let line = line.map_err(|error| error.to_string())?;
         writeln!(&stream, "{}", line).map_err(|error| error.to_string())?;
         let mut response = String::new();
-        reader
+        connection_reader
             .read_line(&mut response)
             .map_err(|error| error.to_string())?;
         let response = response.split("::");
